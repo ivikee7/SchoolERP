d14516dac7622b1321a79983c18fe197950a027b
ivikee7 <80277577+ivikee7@users.noreply.github.com>
2024-04-08 09:00:15 +0530

    public api

--- app/Livewire/Meta/Whatsapp.php
+++ app/Livewire/Meta/Whatsapp.php
@@ -25,21 +25,22 @@ class Whatsapp extends Component
 
     public function whatsappMessageSend()
     {
-        $api_endpoint = "https://graph.facebook.com/v18.0/194604707068893/messages";
-        $api_token = "EAAUT0R9NT8MBOZCCTuE3EZBzFRW3yWMaYiKb2fKGdLAOb90RVgeZBUfGASPUuCZA23Ikq4j1M76ZAStnNZCsbxsDhsZAKDHqJTm9p1DWZAUigPTmZAZBsnHczfxbKWaA9IjDviLTnReYjswyYXZAAIziNAHWrVX6SYxn5rLuzh93IFwHeCmEu3tgWeZBiOumzok1tE9qoUJB0fAMwjiSbN3SUKIU7ZBf85HOkgMwz74UZD";
+        $api_endpoint = "https://graph.facebook.com/v18.0/190963000769830/messages";
+        $api_token = "EAAUT0R9NT8MBOwZBOwwfwexagOtvUIoTMSG2SZC2e18bZBY7at8qIwkG3UR4AyllCk3vs7gzEq4WHn0LQZABLTF8ul9qRl6ebQ87qpxo9fRJDlPvS1FfyoSStLx4t6514Nh3BljceK1Vj7ZAGvkrKrVVLqcQF4x4K2obOOaZAEda7kL1BCakZAjD4UHOVZCKkeloJfXBdy1v8tWH040vMP3g0rmk5kSsAL8CFnMZD";
         $number = $this->whatsapp_numbers; //"+919714396349";
         $message = $this->whatsapp_message; //"https://chat.whatsapp.com/LV03KKA0vG36LYBf0FBvOa";
+        $template_name = $this->template;
 
         $numbers = array_filter(explode(',', str_replace(array("\n", "\r", ",", ".", "|", ":", " ", ",,", "  "), ',', $number)));
 
         foreach ($numbers as $n) {
-            $response = self::send($api_endpoint, $api_token, $n, $message);
+            $response = self::send($api_endpoint, $api_token, $n, $template_name, $message);
             dd($response);
         }
         return $response;
     }
 
-    private function send($api_endpoint, $api_token, $whatsapp_number, $whatsapp_message)
+    private function send($api_endpoint, $api_token, $whatsapp_number, $template_name, $whatsapp_message)
     {
         $phone_number = $whatsapp_number;
         $message_text = $whatsapp_message;
@@ -52,7 +53,7 @@ class Whatsapp extends Component
             "to" => $phone_number,
             "type" => $this->type,
             "template" => [
-                "name" => "join_our_community_page",
+                "name" => $template_name,
                 "language" => [
                     "code" => "en_US"
                 ]
