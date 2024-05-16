9079d5591e6b834067203aa748cbc1bb20eb2e91
ivikee7 <80277577+ivikee7@users.noreply.github.com>
2024-05-16 11:37:25 +0530

    Profile Image

--- app/Livewire/ImageController.php
+++ app/Livewire/ImageController.php
@@ -7,6 +7,8 @@ use App\Livewire\Alert\Notification;
 use App\Models\Media;
 use App\Models\User;
 use Google\Service\Classroom;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Livewire\WithFileUploads;
@@ -34,23 +36,46 @@ class ImageController extends Component
             return Notification::alert($this, 'warning', 'Select image!', "Please select the image file!");
         }
 
-        $image = $this->image->storeAs('public/media/image/user', $id . '_' . $user_full_name . '_' . date('c', time()) . '.' . $this->image->getClientOriginalExtension());
-
-        $file_name = basename($image);
         $path = "storage/media/image/user";
+        $image_ext = $this->image->getClientOriginalExtension();
+        $image = $this->image->storeAs('public/media/image/user', $id . '_' . $user_full_name . '_' . date('c', time()) . '.' . $image_ext);
+        $image_name = basename($image);
+        $image_url = $path . '/' . $image_name;
 
         if (!empty($user_details->media_id)) {
-            return Notification::alert($this, 'warning', 'Image upload!', "media already exists!");
+            if (Media::findOrFail($user_details->media_id)->exists()) {
 
-            // if (Media::findOrFail($user_details->media_id)->exists()) {
-            //     $media = Media::findOrFail($user_details->media_id);
-            // }
+                $media = Media::findOrFail($user_details->media_id);
+                $media->media_path = $image_url;
+                $media->save();
+
+                $user = User::findOrFail($id);
+                $user->media_id = $media->id;
+                $user->save();
+
+                return Notification::alert($this, 'success', 'Image uploaded!', "Image successfully uploaded!");
+            } elseif (!Media::findOrFail($user_details->media_id)->exists()) {
+
+                $media = Media::create([
+                    'media_title' => $id,
+                    'media_path' => $image_url,
+                    'media_type' => 'image',
+                    'created_by' => auth()->user()->id,
+                    'updated_by' => auth()->user()->id,
+                ]);
+
+                $user = User::findOrFail($id);
+                $user->media_id = $media->id;
+                $user->save();
+
+                return Notification::alert($this, 'warning', 'Media and image updated!', "Media and image successfully uploaded!");
+            }
         }
 
         if (empty($user_details->media_id)) {
             $media = Media::create([
                 'media_title' => $id,
-                'media_path' => $path . '/' . $file_name,
+                'media_path' => $image_url,
                 'media_type' => 'image',
                 'created_by' => auth()->user()->id,
                 'updated_by' => auth()->user()->id,
